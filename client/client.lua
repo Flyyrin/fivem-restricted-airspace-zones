c670217b6f96770de940f5c401d8843417796d8c
Flyyrin <66778328+Flyyrin@users.noreply.github.com>
2021-02-21 23:05:21 +0100

    Added audio

--- client/client.lua
+++ client/client.lua
@@ -1,8 +1,26 @@
-RegisterCommand("test", function()
-    if Config.WarningMessage then
-        Citizen.Wait(1)
-        notify(Config.WarningMessageText)
-        TriggerEvent("seatbelt:sounds", "unbuckle", Config.Volume)
-    end    
+AddEventHandler('seatbelt:sounds', function(soundFile, soundVolume)
+  SendNUIMessage({
+    transactionType = 'playSound',
+    transactionFile = soundFile,
+    transactionVolume = soundVolume
+  })
 end)
-    
+
+-- local Warns = {'warn1', 'warn2', 'warn3' 'warn4', 'warn5'}
+-- local Warns2 = {'warn2_1', 'warn2_2', 'warn2_3' 'warn2_4', 'warn2_5'}
+-- local Attacks = {'attack1', 'attack2', 'attack3' 'attack4', 'attack5', 'attack5', 'attack6'}
+-- local Alloweds = {'allowed1', 'allowed2', 'allowed3' 'allowed4'}
+
+
+RegisterCommand("test", function()
+  if Config.WarningMessage then
+      Citizen.Wait(1)
+      notify(Config.WarningMessageText)
+      TriggerEvent("seatbelt:sounds", "warn1", Config.Volume)
+      Citizen.Wait(5000)
+      TriggerEvent("seatbelt:sounds", "warn2_1", Config.Volume)
+  end    
+end)
+
+
+  
--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,12 +1,20 @@
-fx_version 'cerulean'
-games { 'rdr3', 'gta5' }
+fx_version 'adamant'
+game 'gta5'
 
 author 'Flyyrin'
 description 'Set Restricted Airspace Zones'
 version '1.0.0'
 
+ui_page 'html/ui.html'
+
 client_script {
   './client/client.lua',
   'config.lua',
-  './client/functions.lua'
+  './client/functions.lua',
+  
+}
+
+files {
+  'html/*',
+  'html/sounds/*.ogg'
 }
