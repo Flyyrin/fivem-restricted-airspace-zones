00f7c8c1ba91cf48d140689238b36497531ddcd3
Flyyrin <66778328+Flyyrin@users.noreply.github.com>
2021-02-22 20:37:02 +0100

    Deleted Useless comments

--- client/client.lua
+++ client/client.lua
@@ -6,12 +6,6 @@ AddEventHandler('seatbelt:sounds', function(soundFile, soundVolume)
   })
 end)
 
--- local Warns = {'warn1', 'warn2', 'warn3' 'warn4', 'warn5'}
--- local Warns2 = {'warn2_1', 'warn2_2', 'warn2_3' 'warn2_4', 'warn2_5'}
--- local Attacks = {'attack1', 'attack2', 'attack3' 'attack4', 'attack5', 'attack5', 'attack6'}
--- local Alloweds = {'allowed1', 'allowed2', 'allowed3' 'allowed4'}
-
-
 RegisterCommand("warn", function()
   if Config.WarningMessage then
       Citizen.Wait(1)
